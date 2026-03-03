afb55390d65ab93f49b5c94e788a29d0dd9dbe0f
isa-capa <icapla.92@gmail.com>
2026-03-03 16:58:22 -0600

    java springboot

---
+++ socialNetwork/src/test/java/org/generation/socialNetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.socialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
